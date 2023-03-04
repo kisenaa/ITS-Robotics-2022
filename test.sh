04fb04f6b6c1ecee710254c446d4cef9520a07bb
Johannes D <115395772+kisenaa@users.noreply.github.com>
2023-03-04 08:49:53 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env -S bash -e
 
 cat << EOF >> /mnt/@/.snapshots/1/info.xml
-
 <?xml version="1.0"?>
-
 <snapshot>
-
-<type>single</type>
-
-<num>1</num>
-
-<date>1999-03-31 0:00:00</date>
-
-<description>First Root Filesystem</description>
-
-<cleanup>number</cleanup>
-
+  <type>single</type>
+  <num>1</num>
+  <date>1999-03-31 0:00:00</date>
+  <description>First Root Filesystem</description>
+  <cleanup>number</cleanup>
 </snapshot>
-
 EOF
-
